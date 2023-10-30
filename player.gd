a6df5a0b30eb51f99746465b7aac5d684f830ce3
Seyfab <seyfab@hotmail.fr>
2023-10-30 23:54:46 +0100

    first commit

---
+++ player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+@export var ACCELERATION = 500
+@export var MAX_SPEED = 200
+@export var FRICTION  = 500
+
+func _physics_process(delta):
+	move(delta)
+
+func move(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	move_and_slide()
